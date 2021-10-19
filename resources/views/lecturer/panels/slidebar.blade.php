82b7cf28ee7b8fcab374f5844582de5bab0fb2ad
trantiendat <hoangvu.tr2518@gmail.com>
2021-10-19 18:48:15 +0700

    Them moi giao dien quan ly tai khoan giang vien

--- resources/views/lecturer/panels/slidebar.blade.php
+++ resources/views/lecturer/panels/slidebar.blade.php
@@ -44,24 +44,80 @@
 			</ul>
 		</div>
 	</div>
+
 	<div class="panel-body tabs-menu-body side-tab-body p-0 border-0 ">
 		<div class="tab-content">
 			<div class="tab-pane active " id="index1">
 				<ul class="side-menu toggle-menu">
+
+					<li class="mb-2"><span class="side-menu__label"><strong>&mdash; Trang chủ &mdash;</strong></span></li>
 					<li class="slide">
-						<a class="side-menu__item"  data-toggle="slide" href="#"><i class="side-menu__icon typcn typcn-device-desktop"></i><span class="side-menu__label">Dashboard</span></a>
+						<a class="side-menu__item"  data-toggle="slide" href="{{ route('dashboard.index') }}">
+							<i class="side-menu__icon typcn typcn-device-desktop"></i>
+							<span class="side-menu__label">Dashboard</span>
+						</a>
+					</li>
+
+					<li class="mt-5 mb-2"><span class="side-menu__label"><strong>&mdash; Quản lý tài khoản &mdash;</strong></span></li>
+					<li class="slide">
+						<a class="side-menu__item"  data-toggle="slide" href="#">
+							<i class="side-menu__icon fa fa-users"></i>
+							<span class="side-menu__label">Giảng viên</span>
+						</a>
 					</li>
 					<li class="slide">
-						<a class="side-menu__item" data-toggle="slide" href="#"><i class="side-menu__icon typcn typcn-info-outline"></i><span class="side-menu__label">Error Pages</span><i class="angle fa fa-angle-right"></i></a>
+						<a class="side-menu__item"  data-toggle="slide" href="#">
+							<i class="side-menu__icon fa fa-graduation-cap"></i>
+							<span class="side-menu__label">Sinh viên</span>
+						</a>
+					</li>
+
+					<li class="mt-5 mb-2"><span class="side-menu__label"><strong>&mdash; Quản lý chung &mdash;</strong></span></li>
+					<li class="slide">
+						<a class="side-menu__item"  data-toggle="slide" href="#">
+							<i class="side-menu__icon fa fa-th-large"></i>
+							<span class="side-menu__label">Khoa</span>
+						</a>
+					</li>
+					<li class="slide">
+						<a class="side-menu__item"  data-toggle="slide" href="#">
+							<i class="side-menu__icon fa fa-th-list"></i>
+							<span class="side-menu__label">Chuyên ngành</span>
+						</a>
+					</li>
+					<li class="slide">
+						<a class="side-menu__item"  data-toggle="slide" href="#">
+							<i class="side-menu__icon fa fa-book"></i>
+							<span class="side-menu__label">Khóa học</span>
+						</a>
+					</li>
+					<li class="slide">
+						<a class="side-menu__item"  data-toggle="slide" href="#">
+							<i class="side-menu__icon fa fa-file-text"></i>
+							<span class="side-menu__label">Thủ tục sinh viên</span>
+						</a>
+					</li>
+					<li class="slide">
+						<a class="side-menu__item"  data-toggle="slide" href="#">
+							<i class="side-menu__icon fa fa-money"></i>
+							<span class="side-menu__label">Học phí sinh viên</span>
+						</a>
+					</li>
+					<li class="slide">
+						<a class="side-menu__item"  data-toggle="slide" href="#">
+							<i class="side-menu__icon fa fa-newspaper-o"></i>
+							<span class="side-menu__label">Bài viết - Tin tức</span>
+						</a>
+					</li>
+
+
+					{{-- <li class="slide">
+						<a class="side-menu__item" data-toggle="slide" href="#"><i class="side-menu__icon typcn typcn-info-outline"></i><span class="side-menu__label">Khoa</span><i class="angle fa fa-angle-right"></i></a>
 						<ul class="slide-menu">
 							<li><a href="400.html" class="slide-item"> 400 Error</a></li>
-							<li><a href="401.html" class="slide-item"> 401 Error</a></li>
-							<li><a href="403.html" class="slide-item"> 403 Error</a></li>
-							<li><a href="404.html" class="slide-item"> 404 Error</a></li>
-							<li><a href="500.html" class="slide-item"> 500 Error</a></li>
-							<li><a href="503.html" class="slide-item"> 503 Error</a></li>
 						</ul>
-					</li>
+					</li> --}}
+					<br>
 				</ul>
 			</div>
 		</div>
