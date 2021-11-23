7545f91e1728b611bcbc8db7a742155b7eedfefe
nguyentrongtin <tin.187pm14019@vanlanguni.vn>
2021-11-23 16:06:17 +0700

    Hoan thanh loc bai viet theo tac gia

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,12 +10,15 @@ import Form from 'vform'
 import notify from './notify.js'
 import formatDatetime from './format-datetime.js'
 import compo from './component.js'
+import router from './routes.js'
 
 window.Form = Form
 window.Vue = require('vue').default;
 
 Vue.config.productionTip = false
+Vue.prototype.$userId = document.querySelector("meta[name='user-fullname']").getAttribute('content');
 
 const app = new Vue({
     el: '#app',
+    router
 });
