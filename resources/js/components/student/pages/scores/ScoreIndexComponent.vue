4713f3fb8f86ee431ce47944790bf30c7d4bf84f
nguyentuankiet <kietnguyen20101010@gmail.com>
2022-02-22 02:50:20 +0700

    Sua loi xem diem tong khi xem diem theo tung hoc ky

--- resources/js/components/student/pages/scores/ScoreIndexComponent.vue
+++ resources/js/components/student/pages/scores/ScoreIndexComponent.vue
@@ -23,16 +23,24 @@
             </div>
 
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-10">
                     <div class="form-group">
                         <input type="radio" value="1" v-model="view_score" /> Xem điểm sinh viên
                         <input type="radio" value="2" v-model="view_score" /> Xem điểm theo CTĐT
                     </div>
                 </div>
+                <div class="col-md-2" v-show="view_score == 1">
+                    <div class="form-group form-select">
+                        <select class="form-control" v-model="filter">
+                            <option value="">Xem tất cả</option>
+                            <option v-for="i in semesters" :key="i" :value="i">Học kỳ {{ i }}</option>
+                        </select>
+                    </div>
+                </div>
             </div>
 
             <div v-show="view_score == 1" class="table-responsive">
-                <table class="table table-nowrap">
+                <table class="table table-nowrap" v-if="filter == ''">
                     <thead class="blue-background text-white">
                         <tr>
                             <th class="text-center text-white">
@@ -77,6 +85,7 @@
                             </th>
                         </tr>
                     </thead>
+
                     <tbody v-for="i in semesters" :key="i">
                         <tr>
                             <td colspan="14">
@@ -222,6 +231,209 @@
                             </td>
                         </tr>
                     </tbody>
+
+                    <tfoot>
+                        <tr v-show="!semesters.length">
+                            <td colspan="14">
+                                <div class="alert alert-danger">
+                                    Không tìm thấy kết quả phù hợp!
+                                </div>
+                            </td>
+                        </tr>
+                    </tfoot>
+                </table>
+                <table class="table table-nowrap" v-else>
+                    <thead class="blue-background text-white">
+                        <tr>
+                            <th class="text-center text-white">
+                                STT
+                            </th>
+                            <th class="text-center text-white">
+                                Mã môn học
+                            </th>
+                            <th class="text-center text-white">
+                                Tên môn học
+                            </th>
+                            <th class="text-center">
+                                Số TC
+                            </th>
+                            <th class="text-center">
+                                BT (%)
+                            </th>
+                            <th class="text-center">
+                                KT (%)
+                            </th>
+                            <th class="text-center">
+                                CK (%)
+                            </th>
+                            <th></th>
+                            <th class="text-center">
+                                Điểm BT
+                            </th>
+                            <th class="text-center">
+                                Điểm KT
+                            </th>
+                            <th class="text-center">
+                                Điểm CK
+                            </th>
+                            <th class="text-center">
+                                Điểm Tổng
+                            </th>
+                            <th class="text-center">
+                                Điểm chữ
+                            </th>
+                            <th class="text-center">
+                                kết quả
+                            </th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td colspan="14">
+                                <b> Học kỳ {{ filter }} </b>
+                            </td>
+                        </tr>
+
+                        <tr v-show="subjects[filter].length" v-for="(subject, index) in subjects[filter]" :key="subject.subject_id">
+                            <td class="text-center">
+                                {{ (index += 1) }}
+                            </td>
+                            <td class="text-center">
+                                {{ subject.subject_code }}
+                            </td>
+                            <td>
+                                {{ subject.subject_name }}
+                            </td>
+                            <td class="text-center">
+                                {{ subject.subject_credit }}
+                            </td>
+                            <td class="text-center">
+                                {{ subject.subject_score_exercise }}
+                            </td>
+                            <td class="text-center">
+                                {{ subject.subject_score_exam }}
+                            </td>
+                            <td class="text-center">
+                                {{ subject.subject_score_final }}
+                            </td>
+                            <td></td>
+                            <td class="text-center">
+                                {{ subject.register_subject_exercise }}
+                            </td>
+                            <td class="text-center">
+                                {{ subject.register_subject_exam }}
+                            </td>
+                            <td class="text-center">
+                                {{ subject.register_subject_final }}
+                            </td>
+                            <td class="text-center">
+                                <div
+                                    v-if="
+                                        subject.register_subject_exercise == null &&
+                                            subject.register_subject_exam == null &&
+                                            subject.register_subject_final == null
+                                    "
+                                >
+                                    <i class="fa fa-minus" aria-hidden="true"></i>
+                                </div>
+                                <div v-else>
+                                    {{ scoreSum(subject) }}
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                <div
+                                    v-if="
+                                        subject.register_subject_exercise == null &&
+                                            subject.register_subject_exam == null &&
+                                            subject.register_subject_final == null
+                                    "
+                                >
+                                    <i class="fa fa-minus" aria-hidden="true"></i>
+                                </div>
+                                <div v-else>
+                                    <div v-if="scoreSum(subject) < 4">
+                                        F
+                                    </div>
+                                    <div v-else-if="scoreSum(subject) < 5">
+                                        D
+                                    </div>
+                                    <div v-else-if="scoreSum(subject) < 5.5">
+                                        D+
+                                    </div>
+                                    <div v-else-if="scoreSum(subject) < 6.5">
+                                        C
+                                    </div>
+                                    <div v-else-if="scoreSum(subject) < 7">
+                                        C+
+                                    </div>
+                                    <div v-else-if="scoreSum(subject) < 8">
+                                        B
+                                    </div>
+                                    <div v-else-if="scoreSum(subject) < 8.5">
+                                        B+
+                                    </div>
+                                    <div v-else-if="scoreSum(subject) < 9">
+                                        A
+                                    </div>
+                                    <div v-else-if="scoreSum(subject) <= 10">
+                                        A+
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="text-center">
+                                <div
+                                    v-if="
+                                        subject.register_subject_exercise == null &&
+                                            subject.register_subject_exam == null &&
+                                            subject.register_subject_final == null
+                                    "
+                                >
+                                    <i class="fa fa-minus" aria-hidden="true"></i>
+                                </div>
+                                <div v-else>
+                                    <div v-if="scoreSum(subject) < 4">
+                                        <i class="fa fa-times" aria-hidden="true"></i>
+                                    </div>
+                                    <div v-else-if="scoreSum(subject) >= 4">
+                                        <i class="fa fa-check" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr class="background-result">
+                            <td colspan="7">
+                                <div>
+                                    Số TC đạt được trong học kỳ:
+                                    <b>
+                                        {{ resultCreditSemester(filter) }}
+                                    </b>
+                                </div>
+                                <div>
+                                    Điểm TB học kỳ:
+                                    <b>
+                                        {{ resultScoreSemester(filter) }}
+                                    </b>
+                                </div>
+                            </td>
+                            <td></td>
+                            <td colspan="6">
+                                <div>
+                                    Số TC tích lũy:
+                                    <b>
+                                        {{ resultCreditAll(filter) }}
+                                    </b>
+                                </div>
+                                <div>
+                                    Điểm TB tích lũy:
+                                    <b>
+                                        {{ resultScoreAll(filter) }}
+                                    </b>
+                                </div>
+                            </td>
+                        </tr>
+                    </tbody>
+
                     <tfoot>
                         <tr v-show="!semesters.length">
                             <td colspan="14">
@@ -379,7 +591,8 @@ export default {
             subjects_programs: [],
             semesters_program: [],
             scores_program: [],
-            error: {}
+            error: {},
+            filter: ''
         }
     },
     mounted() {
