a88d6cc3cd1b568414c5ee0bdf9a65aaf367ac02
phanphungvotin <phanphungtin@gmail.com>
2022-03-06 15:01:26 +0700

    Tao moi giao dien xem lich thi cho sinh vien

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -78,6 +78,16 @@ import StudyPlanSuggestIndex from './components/admin/pages/suggest/StudyPlanSug
 import StudyPlanSuggestCreate from './components/admin/pages/suggest/StudyPlanSuggestCreateComponent.vue'
 import StudyPlanSuggestUpdate from './components/admin/pages/suggest/StudyPlanSuggestUpdateComponent.vue'
 
+import StatisticPLan from './components/admin/pages/statistic_plan/StatisticComponent.vue'
+import StatisticPLanIndex from './components/admin/pages/statistic_plan/StatisticIndexComponent.vue'
+import StatisticPLanStudent from './components/admin/pages/statistic_plan/StatisticStudentComponent.vue'
+import StatisticPLanType from './components/admin/pages/statistic_plan/StatisticTypeComponent.vue'
+
+import StatisticSubject from './components/admin/pages/statistic_subject/StatisticSubjectComponent.vue'
+import StatisticSubjectIndex from './components/admin/pages/statistic_subject/StatisticSubjectIndexComponent.vue'
+import StatisticSubjectDetail from './components/admin/pages/statistic_subject/StatisticSubjectDetailComponent.vue'
+
+//---------------------------------- STUDENT --------------------------------------------------------
 import Home from './components/student/pages/HomeComponent.vue'
 
 import StudyPlan from './components/student/pages/study_plan/StudyPlanComponent.vue'
@@ -131,6 +141,7 @@ export default new VueRouter({
             }
         },
 
+        //-------------------- ADMIN -------------------
         {
             path: '/giang-vien',
             name: 'lecturer',
@@ -254,6 +265,75 @@ export default new VueRouter({
         },
 
         {
+            path: '/thu-tuc',
+            name: 'procedure',
+            component: Procedure,
+            children: [
+                {
+                    path: '',
+                    name: 'procedureindex',
+                    component: ProcedureIndex
+                },
+
+                {
+                    path: 'tao-moi',
+                    name: 'procedurecreate',
+                    component: ProcedureCreate
+                },
+
+                {
+                    path: 'cap-nhat/:idProcedure',
+                    name: 'procedureupdate',
+                    component: ProcedureUpdate
+                },
+
+                {
+                    path: 'yeu-cau-cua-sinh-vien',
+                    name: 'procedurerequire',
+                    component: ProcedureRequire
+                }
+            ],
+            beforeEnter: (to, from, next) => {
+                if (Vue.prototype.$adminId != null) {
+                    next()
+                } else {
+                    next(false)
+                }
+            }
+        },
+
+        {
+            path: '/thong-tin-ca-nhan',
+            name: 'info',
+            component: Info,
+            children: [
+                {
+                    path: '',
+                    name: 'infoindex',
+                    component: InfoIndex
+                },
+                {
+                    path: 'nhap-thong-tin',
+                    name: 'infocreate',
+                    component: InfoCreate
+                },
+                {
+                    path: 'cap-nhat/:idAdmin',
+                    name: 'infoupdate',
+                    component: InfoUpdate
+                }
+            ],
+            beforeEnter: (to, from, next) => {
+                if (Vue.prototype.$adminId != null) {
+                    next()
+                } else {
+                    next(false)
+                }
+            }
+        },
+
+        //---------------------- DEAN OF FACULTY ---------------------
+        {
             path: '/mon-hoc',
             name: 'subject',
             component: Subject,
@@ -310,44 +390,6 @@ export default new VueRouter({
             ],
             beforeEnter: (to, from, next) => {
                 if (Vue.prototype.$facultyId != null) {
-                    next()
-                } else {
-                    next(false)
-                }
-            }
-        },
-
-        {
-            path: '/thu-tuc',
-            name: 'procedure',
-            component: Procedure,
-            children: [
-                {
-                    path: '',
-                    name: 'procedureindex',
-                    component: ProcedureIndex
-                },
-
-                {
-                    path: 'tao-moi',
-                    name: 'procedurecreate',
-                    component: ProcedureCreate
-                },
-
-                {
-                    path: 'cap-nhat/:idProcedure',
-                    name: 'procedureupdate',
-                    component: ProcedureUpdate
-                },
-
-                {
-                    path: 'yeu-cau-cua-sinh-vien',
-                    name: 'procedurerequire',
-                    component: ProcedureRequire
-                }
-            ],
-            beforeEnter: (to, from, next) => {
-                if (Vue.prototype.$adminId != null) {
                     next()
                 } else {
                     next(false)
@@ -436,61 +478,6 @@ export default new VueRouter({
         },
 
         {
-            path: '/chuong-trinh-dao-tao-sinh-vien',
-            name: 'educationprogramstudent',
-            component: EducationProgramStudent,
-            children: [
-                {
-                    path: '',
-                    name: 'educationprogramstudentindex',
-                    component: EducationProgramStudentIndex
-                },
-                {
-                    path: ':idEPStudent',
-                    name: 'educationprogramstudentdetail',
-                    component: EducationProgramStudentDetail
-                }
-            ],
-            beforeEnter: (to, from, next) => {
-                if (Vue.prototype.$teacherId != null) {
-                    next()
-                } else {
-                    next(false)
-                }
-            }
-        },
-
-        {
-            path: '/thong-tin-ca-nhan',
-            name: 'info',
-            component: Info,
-            children: [
-                {
-                    path: '',
-                    name: 'infoindex',
-                    component: InfoIndex
-                },
-                {
-                    path: 'nhap-thong-tin',
-                    name: 'infocreate',
-                    component: InfoCreate
-                },
-                {
-                    path: 'cap-nhat/:idAdmin',
-                    name: 'infoupdate',
-                    component: InfoUpdate
-                }
-            ],
-            beforeEnter: (to, from, next) => {
-                if (Vue.prototype.$adminId != null) {
-                    next()
-                } else {
-                    next(false)
-                }
-            }
-        },
-
-        {
             path: '/thong-tin-ca-nhan-cua-ban-chu-nhiem-khoa',
             name: 'infolecturer',
             component: InfoLecturer,
@@ -520,6 +507,62 @@ export default new VueRouter({
             }
         },
 
+        {
+            path: '/thong-ke-so-luong-dang-ky-ke-hoach',
+            name: 'statisticplan',
+            component: StatisticPLan,
+            children: [
+                {
+                    path: '',
+                    name: 'statisticindex',
+                    component: StatisticPLanIndex
+                },
+                {
+                    path: 'chi-tiet-dang-ky-ke-hoach-sinh-vien/:idCourse/:idMajor/:idSemester',
+                    name: 'statisticplanstudent',
+                    component: StatisticPLanStudent
+                },
+                {
+                    path: 'chi-tiet-dang-ky-loai-ke-hoach-sinh-vien/:idCourse/:idMajor/:idSemester',
+                    name: 'statisticplantype',
+                    component: StatisticPLanType
+                }
+            ],
+            beforeEnter: (to, from, next) => {
+                if (Vue.prototype.$facultyId != null) {
+                    next()
+                } else {
+                    next(false)
+                }
+            }
+        },
+
+        {
+            path: '/thong-ke-so-luong-dang-ky-mon-hoc',
+            name: 'statisticsubject',
+            component: StatisticSubject,
+            children: [
+                {
+                    path: '',
+                    name: 'statisticsubjectindex',
+                    component: StatisticSubjectIndex
+                },
+                {
+                    path: 'chi-tiet-dang-ky-mon-hoc-sinh-vien/:idCourse/:idMajor/:idSemester',
+                    name: 'statisticsubjectdetail',
+                    component: StatisticSubjectDetail
+                }
+            ],
+            beforeEnter: (to, from, next) => {
+                if (Vue.prototype.$facultyId != null) {
+                    next()
+                } else {
+                    next(false)
+                }
+            }
+        },
+
+        //-------------------------- FORM TEACHER -------------------
         {
             path: '/thong-tin-ca-nhan-cua-chu-nhiem-sinh-vien',
             name: 'infoformteacher',
@@ -635,6 +678,32 @@ export default new VueRouter({
             }
         },
 
+        {
+            path: '/chuong-trinh-dao-tao-sinh-vien',
+            name: 'educationprogramstudent',
+            component: EducationProgramStudent,
+            children: [
+                {
+                    path: '',
+                    name: 'educationprogramstudentindex',
+                    component: EducationProgramStudentIndex
+                },
+                {
+                    path: ':idEPStudent',
+                    name: 'educationprogramstudentdetail',
+                    component: EducationProgramStudentDetail
+                }
+            ],
+            beforeEnter: (to, from, next) => {
+                if (Vue.prototype.$teacherId != null) {
+                    next()
+                } else {
+                    next(false)
+                }
+            }
+        },
+
+        //-------------------- STUDENT ----------------------------
         {
             path: '/ke-hoach-hoc-tap',
             name: 'studyplan',
